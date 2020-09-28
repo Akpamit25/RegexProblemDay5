ec7634a88a64eff0cc66ff32515a16dd958251cb
Amit Kumar <amitnitsri25@gmail.com>
2020-09-28 08:02:34 +0530

    added

---
+++ src/com/capgemini/UC5.java
@@ -0,0 +1,27 @@
+package com.capgemini;
+
+import java.util.regex.*; 
+import java.util.*;
+
+public class UC5 {
+
+	public static boolean isValidPassword(String pswd) 
+	{ 
+		String regex = "^[a-zA-Z0-9\\@\\_\\-\\+\\#\\*]{8,}$";
+		Pattern p = Pattern.compile(regex); 
+		Matcher m = p.matcher(pswd); 
+		return m.matches(); 
+	} 
+
+	public static void main(String args[]) 
+	{ 
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter a password: ");
+		String pswd = sc.nextLine();
+		if(isValidPassword(pswd)){
+			System.out.println("PASSWORD ACCEPTED");
+		}else{
+			System.out.println("Password rejected, retry again");
+		}
+	} 
+}
